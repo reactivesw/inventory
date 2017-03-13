c284e98bb83b13534007c199c910734deb648625
Davis <davis.dai@go6d.com>
2017-03-13 17:41:47 +0800

    refactor

--- src/main/java/io/reactivesw/inventory/domain/model/InventoryEntry.java
+++ src/main/java/io/reactivesw/inventory/domain/model/InventoryEntry.java
@@ -5,11 +5,13 @@ import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.ZonedDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -22,6 +24,7 @@ import javax.persistence.Version;
 @EqualsAndHashCode(callSuper = false)
 @Entity
 @Table(name = "inventory_entry")
+@EntityListeners(AuditingEntityListener.class)
 public class InventoryEntry {
 
 
