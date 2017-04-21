5b251643c316f8bb3b33a94612c72e7248d22b34
Davis <davis.dai@go6d.com>
2017-04-21 17:32:50 +0800

    fix autowired bug

--- src/main/java/io/reactivesw/inventory/application/service/OrderCreationConsumer.java
+++ src/main/java/io/reactivesw/inventory/application/service/OrderCreationConsumer.java
@@ -10,6 +10,7 @@ import io.reactivesw.message.client.utils.serializer.JsonDeserializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +35,7 @@ public class OrderCreationConsumer {
   /**
    * OrderCreationEvent handler.
    */
+  @Autowired
   private transient OrderCreationEventHandler eventHandler;
 
   /**
